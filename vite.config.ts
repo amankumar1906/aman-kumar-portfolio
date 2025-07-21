507fea277c13901fa2d057a8e201baa4f5bd280e
amankumar1906 <amankumar7506@gmail.com>
2025-07-20 21:38:16 -0400

    add deployment fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aman-kumar-portfolio/',
+  base: '/aman-kumar-portfolio/', // make sure this matches your repo
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
